87ae0cfa38155309cc090dc122be1c7b6b6fc968
genzj <zj0512@gmail.com>
2021-06-20 00:19:33 -0700

    add async task interoperation to tcp protocol

--- 04-protocol-tcp/tcp_server.py
+++ 04-protocol-tcp/tcp_server.py
@@ -4,6 +4,11 @@ import json
 
 
 class AddServerProtocol(asyncio.Protocol):
+
+    async def add(self, a, b):
+        await asyncio.sleep(1)
+        return a + b
+
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
         print('Connection from {}'.format(peername))
@@ -13,9 +18,14 @@ class AddServerProtocol(asyncio.Protocol):
         message = json.loads(data)
         print('Data received: {!r}'.format(message))
 
-        answer = {
-            'answer': message.get('a', 0) + message.get('b', 0)
-        }
+        task = asyncio.create_task(
+            self.add(message.get('a', 0), message.get('b', 0))
+        )
+
+        task.add_done_callback(self.add_done)
+
+    def add_done(self, task):
+        answer = {'answer': task.result()}
         print('Send: {!r}'.format(answer))
         self.transport.write(json.dumps(answer).encode('ascii'))
 
@@ -27,6 +37,7 @@ loop = asyncio.get_event_loop()
 # Each client connection will create a new protocol instance
 coro = loop.create_server(AddServerProtocol, '127.0.0.1', 8888)
 server = loop.run_until_complete(coro)
+assert server.sockets
 
 # Serve requests until Ctrl+C is pressed
 print('Serving on {}'.format(server.sockets[0].getsockname()))
