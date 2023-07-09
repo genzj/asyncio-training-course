951241215659567e4d70aacd9f5d10db0af67b54
genzj <zj0512@gmail.com>
2023-07-08 23:24:31 -0700

    adapt fast api example to new hefeng API

--- 10-fast-api-project/main.py
+++ 10-fast-api-project/main.py
@@ -1,5 +1,4 @@
 import asyncio
-import base64
 from datetime import datetime
 import hashlib
 import aiohttp
@@ -24,8 +23,7 @@ def sign(params, secret):
             to_sign += k + '=' + v + '&'
     to_sign = to_sign[:-1]
     to_sign += secret
-    md5 = hashlib.md5(to_sign.encode('utf-8')).digest()
-    return base64.b64encode(md5).decode('ascii')
+    return hashlib.md5(to_sign.encode('utf-8')).hexdigest()
 
 
 async def fetch(session, url, params):
@@ -33,11 +31,11 @@ async def fetch(session, url, params):
         return await response.json()
 
 
-async def hefeng(session, city):
-    url = 'https://free-api.heweather.com/s6/weather/now'
+async def hefeng(session):
+    url = 'https://devapi.qweather.com/v7/weather/now'
     params = {
-        "location": city,
-        "username": "HE1601211533111623",
+        "location": "101020100",  # LocationID of Shanghai
+        "publicid": "HE1601211533111623",
         "t": str(utctimestamp())
     }
     params['sign'] = sign(params, 'd5bd328dd36844bbb20c0e4905568e8e')
@@ -45,11 +43,11 @@ async def hefeng(session, city):
     return {'source': 'hefeng', 'result': await fetch(session, url, params)}
 
 
-async def openweathermap(session, city):
+async def openweathermap(session):
     url = 'https://api.openweathermap.org/data/2.5/weather'
 
     params = {
-        "q": city,
+        "q": "Shanghai",
         "appid": "18bc2f96c466fc82cd607d43eb152055",
         "units": "metric",
     }
@@ -63,8 +61,8 @@ async def openweathermap(session, city):
 async def root():
     async with aiohttp.ClientSession() as session:
         tasks = {
-            "hefeng": hefeng(session, 'Shanghai'),
-            "openweathermap": openweathermap(session, 'Shanghai')
+            "hefeng": hefeng(session),
+            "openweathermap": openweathermap(session)
         }
         names = list(tasks.keys())
         coroutines = list(tasks.values())
